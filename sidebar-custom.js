2b82fbc2058ef7ec704135ec215f21d904a8996b
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2024-03-09 15:47:11 -0500

    Initial commit

---
+++ sidebar-custom.js
@@ -0,0 +1,4 @@
+window.document.addEventListener("DOMContentLoaded", function (event) {
+    var $subtitleDiv = $("<hr><span class='subtitle'>Georgetown University Graduate School of Arts and Sciences</span>");
+    $('.sidebar-title').append($subtitleDiv);
+  });
